e726e01e31c0df9df84e06d06efa3975113de906
Anish2804 <anishkumar2804@gmail.com>
2026-08-19 12:08:24 +0000

    Integrate SQLAlchemy with FastAPI and SQLite

--- Section_08/FASTAPI/15_sqlAlchemy_db/main.py
+++ Section_08/FASTAPI/15_sqlAlchemy_db/main.py
@@ -4,18 +4,22 @@ from fastapi import FastAPI,Depends
 
 app=FastAPI()
 
+# database url
 DATABASE_URL="sqlite:///./test.db"
 
+# engine create (DB connection)
 engine=create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread":False}
 )
 
+# Session (DB operation le liye)
 sessionLocal=sessionmaker(bind=engine)
 
+# Base (model ke liye)
 Base = declarative_base()
 
-
+# Table (Model)
 class todo(Base):
     __tablename__="todos"
     
@@ -23,10 +27,10 @@ class todo(Base):
     title=Column(String)
     Completed=Column(String)
     
-    
+# Table create  
 Base.metadata.create_all(bind=engine)
 
-
+# Dependency (DB session provide krega)
 def get_db():
     db=sessionLocal()
     try:
