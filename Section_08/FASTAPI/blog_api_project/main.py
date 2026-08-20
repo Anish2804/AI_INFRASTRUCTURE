1ecd592a240c31a8a0cd659759f4d19021cf2708
Anish2804 <anishkumar2804@gmail.com>
2026-08-20 22:26:34 +0000

    Build blog API with PostgreSQL JWT authentication and CRUD operations

---
+++ Section_08/FASTAPI/blog_api_project/main.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI,Depends,HTTPException,Query
+from sqlalchemy.orm import Session
+from .database import engine,SessionLocal
+from . import models,schemas
+from .auth import create_token,verify_token
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+#DB Dependency
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# login api
+@app.post("/login")
+def login():
+    return{
+        "access_token":create_token({"user":"admin"}),
+        "token_type":"bearer"
+    }
+
+# home route
+@app.get("/")
+def home():
+    return{
+        "message":"Blog API started"
+    } 
+    
+    
+# create blog (protected)
+@app.post("/blogs", response_model=schemas.BlogResponse)
+def create_blog(blog:schemas.BlogCreate, db:Session=Depends(get_db), user=Depends(verify_token)):
+    new_blog = models.Blog(
+        title=blog.title,
+        content=blog.content
+    )
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+# read all blog
+@app.get("/blogs")
+def get_blogs(page:int=1 ,
+              limit:int=5, 
+              search:str=Query(default=""), 
+              db:Session=Depends(get_db)):
+    query=db.query(models.Blog)
+    if search:
+        query=query.filter(models.Blog.title.ilike(f"%{search}%"))
+        
+    total= query.count()
+    start=(page-1)*limit
+    blogs=query.offset(start).limit(limit).all()
+    
+    return{
+        "page":page,
+        "limit":limit,
+        "total":total,
+        "data":blogs
+    }
+
+# read one blog
+@app.get("/blogs/{id}",response_model=schemas.BlogResponse)
+def get_blog(id:int,db:Session=Depends(get_db)):
+    blog= db.query(models.Blog).filter(models.Blog.id==id).first()
+    
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return blog
+
+
+
+# update blog api (protected)
+@app.put("/blogs/{id}",response_model=schemas.BlogResponse)
+def update_blog(id:int,blog:schemas.BlogCreate,db:Session=Depends(get_db),user=Depends(verify_token)):
+    existing_blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    
+    if not existing_blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    
+    existing_blog.title = blog.title
+    existing_blog.content=blog.content
+    
+    db.commit()
+    db.refresh(existing_blog)
+    
+    return existing_blog
+
+
+
+# delete blog api (protected)
+@app.delete("/blogs/{id}")
+def delete_blog(id:int,db:Session=Depends(get_db),user=Depends(verify_token)):
+    blog= db.query(models.Blog).filter(models.Blog.id==id)
+    
+    if not blog.first():
+        raise HTTPException(status_code=404, detail="Blog not found")
+    
+    blog.delete()
+    db.commit()
+    #db.refresh(blog)
+    
+    return{
+        "message":"Blog deleted successfully"
+    }
+        
+    
+    
